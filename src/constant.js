dfc63fac53153b4390b20f59410dfefa855377b2
Vishnu1031 <vishumalav256@gmail.com>
2024-08-08 01:33:08 +0530

    created .env file

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,1 @@
-const Google_API_KEY= "AIzaSyCCIr_lCy9tCTxznOV1wxfCmptyBUvtz8g"
-
-export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${Google_API_KEY}`
+export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.React__App__Google_API_KEY}`;
